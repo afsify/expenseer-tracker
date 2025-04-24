bc55f8516dcd3ccb0367d48c0b68580b21d8d2d4
afsify <afsalafs8008@gmail.com>
2025-04-24 18:13:42 +0530

    updated

--- client/src/components/common/Spinner.jsx
+++ client/src/components/common/Spinner.jsx
@@ -9,7 +9,7 @@ function Spinner() {
         <div className="rounded-full h-24 w-24 border-t-4 border-t-blue-500 animate-spin absolute"></div>
         <div className="h-full w-full flex justify-center items-center">
           <h1 className="text-blue-500 text-3xl font-mono font-extrabold">
-            &lt; / &gt;
+            &lt;/&gt;
           </h1>
         </div>
       </div>
